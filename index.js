cb54b2ef2f9c555aead451fc32da51ef70a9dcbd
Your Name <marthaball@ufl.edu>
2022-04-17 20:26:38 -0400

    custom name

--- index.js
+++ index.js
@@ -8,12 +8,39 @@ var textarea = document.querySelector('textarea')
 // Then apply them to elements on the page
 // YOUR CODE HERE
 
+cookieStore.get('name')
+
+  .then(function(cookieObj) {
+
+    if (cookieObj) {
+       nameSpan.textContent = cookieObj.value
+    }
+  })
+
+    var textObj = localStorage.getItem('text')
+
+
+    if (textObj) {
+      textarea.textContent = textObj
+    } else {
+    textarea.textContent = ''
+
+}
+
+
+
 formEl.onsubmit = function(e) {
   // prevents form submission
   e.preventDefault()
   // save name element's content to cookies
   // save textarea's content to localstorage
   // YOUR CODE HERE
+  textInput = textarea.value
+
+  document.cookie = 'name=' +
+   nameSpan.textContent + ';'
+  localStorage.setItem('text', textInput)
+
 
   // triggers thumbs up animation
   this.elements.save.classList.add('emoji')
@@ -23,6 +50,9 @@ clear.onclick = function() {
   // Clear textarea's value
   // Clear localstorage's content
   // YOUR CODE HERE
+  textarea.value = ''
+  localStorage.clear()
+
 
   // triggers thumbs up animation
   this.classList.add('emoji')
